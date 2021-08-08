297a2eccc9dcd621f40c4f0305fe6deb2fd2610e
仿生狮子（一光） <tangnad@qq.com>
2021-08-08 23:22:52 +0800

    feat: 防止 window.name、window.opener 属性泄露隐私

--- src/preload.js
+++ src/preload.js
@@ -1,1 +1,23 @@
 $ = undefined
+
+(function (window) {
+
+  /******************************************************** 防止 window.name、window.opener 属性泄露隐私 */
+
+  const windowNames = new WeakMap()
+  Object.defineProperty(window, 'name', {
+    set(name) {
+      windowNames.set(this, name)
+    },
+    get() {
+      return windowNames.get(this) || ""
+    }
+  })
+  Object.defineProperty(window, 'opener', {
+    value: null,
+    get () {
+      return null
+    }
+  })
+
+})(window)
