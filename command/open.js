5bf4bf17ea2e4f56fe43ae8792b8e756b266ac62
lionad-trident <tangnad@qq.com>
2021-08-28 23:16:52 +0800

    feat: 等待我们的地址栏加载完毕

--- command/open.js
+++ command/open.js
@@ -1,4 +1,9 @@
+const fs = require('fs')
+const path = require('path')
+
+const { waitUntilLoaded } = require('../utils/dom')
 const { getBrowser, utils } = require('../src/chrome')
+const preloadFile = fs.readFileSync(path.join(__dirname, '../src/preload.js'), 'utf8')
 
 const [_, __, url] = process.argv
 console.log('url:', url)
@@ -8,8 +13,17 @@ console.log('url:', url)
     const browser = await getBrowser()
     const page = await browser.newPage()
     await page.setViewport(utils.setViewport())
+    await page.evaluateOnNewDocument(preloadFile)
+    await page.evaluateOnNewDocument(waitUntilLoaded)
+
     await page.goto(url, { waitUntil: 'domcontentloaded' })
+
+    // 等待我们的地址栏加载完毕
+    await page.evaluate(async () => await window.waitUntilLoaded('#addressText'))
+
+
     // await page.screenshot({ path: 'test.png' })
+
   } catch (error) {
     console.log(error)
   }
---
+++ utils/dom.js
@@ -0,0 +1,46 @@
+const waitUntilLoaded = function () {
+  const fn = ($elm, timeout = 500) => {
+    let tick
+    const target = typeof $elm === 'string'
+      ? document.querySelector($elm)
+      : $elm
+    return new Promise((resolve, reject) => {
+      if (!target) {
+        console.log('[INFO] skip wait no elem')
+        resolve()
+      }
+      // 超时报错
+      const errorTick = setTimeout(() => {
+        tick && window.clearTimeout(tick)
+        reject()
+      }, 10 * 1000)
+
+      tick = setTimeout(() => {
+        window.clearTimeout(errorTick)
+        resolve()
+      }, timeout)
+
+      const observer = new window.MutationObserver(list => {
+        console.log(list)
+        tick && window.clearTimeout(tick)
+        tick = setTimeout(() => {
+          window.clearTimeout(errorTick)
+          resolve()
+        }, timeout)
+      })
+      observer.observe(target, {
+        childList: true,
+        subtree: true
+      })
+    })
+  }
+  if (window) {
+    window.waitUntilLoaded = fn
+  } else {
+    return fn
+  }
+}
+
+module.exports = {
+  waitUntilLoaded
+}
