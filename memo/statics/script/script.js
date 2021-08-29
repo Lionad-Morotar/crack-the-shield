f9eb02a55f0c28de63ebe4d8609b156924377c33
lionad-trident <tangnad@qq.com>
2021-08-29 14:39:17 +0800

    stash

---
+++ memo/statics/script/script.js
@@ -0,0 +1,74 @@
+const _0x46a74e = _0x3e98;
+(function (_0x5de88d, _0x1a91fc) {
+  const _0x19fd21 = _0x3e98,
+    _0x12ffeb = _0x5de88d();
+  while (!![]) {
+    try {
+      const _0x386ddd =
+        parseInt(_0x19fd21(0x7a)) / 0x1 +
+        -parseInt(_0x19fd21(0x70)) / 0x2 +
+        (-parseInt(_0x19fd21(0x77)) / 0x3) *
+          (-parseInt(_0x19fd21(0x7c)) / 0x4) +
+        (parseInt(_0x19fd21(0x74)) / 0x5) * (-parseInt(_0x19fd21(0x6e)) / 0x6) +
+        parseInt(_0x19fd21(0x75)) / 0x7 +
+        parseInt(_0x19fd21(0x73)) / 0x8 +
+        -parseInt(_0x19fd21(0x71)) / 0x9;
+      if (_0x386ddd === _0x1a91fc) break;
+      else _0x12ffeb['push'](_0x12ffeb['shift']());
+    } catch (_0x817278) {
+      _0x12ffeb['push'](_0x12ffeb['shift']());
+    }
+  }
+})(_0x2b38, 0x7deba);
+function _0x2b38() {
+  const _0x33b317 = [
+    'stop',
+    'length',
+    '487019SPLCOV',
+    'webdriver',
+    '53272WgNAZV',
+    'charCodeAt',
+    '402lYzVpj',
+    'String',
+    '184566FbtOwC',
+    '2586681FJDgMu',
+    'navigator',
+    '2786400DMdyoH',
+    '43840jbRtHW',
+    '151571bsjvYT',
+    'write',
+    '141qRscZu',
+  ];
+  _0x2b38 = function () {
+    return _0x33b317;
+  };
+  return _0x2b38();
+}
+function _0x3e98(_0x2f420d, _0x1fd0f7) {
+  const _0x2b38fa = _0x2b38();
+  return (
+    (_0x3e98 = function (_0x3e98d2, _0x565551) {
+      _0x3e98d2 = _0x3e98d2 - 0x6d;
+      let _0x8edcd5 = _0x2b38fa[_0x3e98d2];
+      return _0x8edcd5;
+    }),
+    _0x3e98(_0x2f420d, _0x1fd0f7)
+  );
+}
+window[_0x46a74e(0x72)][_0x46a74e(0x7b)] &&
+  (document[_0x46a74e(0x76)]('<span>你不对劲</span>'),
+  window[_0x46a74e(0x78)]());
+function es(_0x380035) {
+  const _0x4301eb = _0x46a74e;
+  let _0x165d09 = '';
+  const _0x35a10a = window['atob'](_0x380035);
+  for (
+    let _0x321545 = 0x0;
+    _0x321545 < _0x35a10a[_0x4301eb(0x79)];
+    _0x321545++
+  ) {
+    const _0x49c453 = _0x35a10a[_0x4301eb(0x6d)](_0x321545);
+    _0x165d09 += window[_0x4301eb(0x6f)]['fromCharCode'](_0x49c453 - 0xa);
+  }
+  return _0x165d09;
+}
